6c10eb2ac029f4abfdd2f956de83f432b239e18a
eugene <eros2@inbox.ru>
2019-01-25 18:52:34 +0200

    formatting

--- src/extension.js
+++ src/extension.js
@@ -9,33 +9,33 @@ let systemMenu, _cancellable, _proxy, _lockActionBtn, _lockActionId, _lockAction
 
 function _onProxyCallFailure(o, res)
 {
-	try {
-		o.call_finish(res);
-	}
+    try {
+        o.call_finish(res);
+    }
     catch ( e ) {
-		// Make one last-ditch attempt by having the D-Bus call done externally
-		Util.spawn( ['gnome-screensaver-command', '-l'] );
-	}
+        // Make one last-ditch attempt by having the D-Bus call done externally
+        Util.spawn( ['gnome-screensaver-command', '-l'] );
+    }
 }
 
 function _onLockClicked() {
-	systemMenu.menu.itemActivated( BoxPointer.PopupAnimation.NONE );
-	_proxy.call( "Lock", null, Gio.DBusCallFlags.NONE, -1, null, _onProxyCallFailure );
+    systemMenu.menu.itemActivated( BoxPointer.PopupAnimation.NONE );
+    _proxy.call( "Lock", null, Gio.DBusCallFlags.NONE, -1, null, _onProxyCallFailure );
 }
 
 function _onProxyReady(o, res)
 {
-	try
+    try
     {
-		_cancellable = null;
+        _cancellable = null;
 
-		_proxy = Gio.DBusProxy.new_finish(res);
+        _proxy = Gio.DBusProxy.new_finish(res);
 
-		_lockActionId = _lockActionBtn.connect('clicked', _onLockClicked);
-	}
+        _lockActionId = _lockActionBtn.connect('clicked', _onLockClicked);
+    }
     catch ( e ) {
         Main.notifyError( 'gnome-screensaver-lock: ' + e );
-	}
+    }
 }
 
 function enable()
@@ -44,8 +44,8 @@ function enable()
     _lockAction.available = false;
     _lockActionBtn = Main.panel.statusArea.aggregateMenu._system._lockScreenAction;
 
-	systemMenu = Main.panel.statusArea['aggregateMenu']._system;
-	_cancellable = new Gio.Cancellable();
+    systemMenu = Main.panel.statusArea['aggregateMenu']._system;
+    _cancellable = new Gio.Cancellable();
     Gio.DBusProxy.new(Gio.DBus.session,
                       Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES | Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS | Gio.DBusProxyFlags.DO_NOT_AUTO_START,
                       null,
@@ -60,7 +60,7 @@ function disable()
 {
     _lockAction.available = true;
     _lockActionBtn.disconnect( _lockActionId );
-	if (_cancellable) {
-		_cancellable.cancel();
-	}
+    if (_cancellable) {
+        _cancellable.cancel();
+    }
 }
