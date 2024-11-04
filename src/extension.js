15dc1714af0f5b2199f692859074c29c8836607a
eugene-rom <eros2@inbox.ru>
2024-11-04 16:07:43 +0200

    cosmetic

--- src/extension.js
+++ src/extension.js
@@ -5,13 +5,13 @@ const Util = imports.misc.util;
 
 let _shieldOnStatusChanged, _shieldActivate, _shieldLock, _shieldDeactivate;
 
-function _lock() { 
+function _lock() {
     Main.overview.hide();
     Main.panel.closeQuickSettings();
     Util.spawn( ['gnome-screensaver-command', '-l'] );
 };
 
-function _unlock() { 
+function _unlock() {
     Util.spawn( ['gnome-screensaver-command', '-d'] );
 };
 
@@ -21,7 +21,7 @@ function enable()
     _shieldActivate = null;
     _shieldLock = null;
     _shieldDeactivate = null;
-    
+
     let _lockActionBtn = null;
     let arr = Main.panel.statusArea.quickSettings._system._systemItem.child.get_children();
     for (i = 0; i < arr.length; i++) {
@@ -38,10 +38,10 @@ function enable()
         _shieldLock = Main.screenShield.lock;
         _shieldDeactivate = Main.screenShield.deactivate;
 
-        Main.screenShield._onStatusChanged = (status) => {}; // prevents gnome shell message "error: Unable to lock: Lock was blocked by an application"  
-        Main.screenShield.activate = (animate) => { _lock() };
-        Main.screenShield.lock = (animate) => { _lock() };
-        Main.screenShield.deactivate = (animate) => { _unlock() };
+        Main.screenShield._onStatusChanged = (status) => {}; // prevents gnome shell message "error: Unable to lock: Lock was blocked by an application"
+        Main.screenShield.activate = (animate) => { _lock(); };
+        Main.screenShield.lock = (animate) => { _lock(); };
+        Main.screenShield.deactivate = (animate) => { _unlock(); };
 
         _lockActionBtn.visible = true;
     }
