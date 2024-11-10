b8d7ce0d2d4f272c7c6f862d8ca0e9d019eda04a
eugene <eros2@inbox.ru>
2024-11-10 16:31:00 +0200

    gnome 42 with gdm

--- src/extension.js
+++ src/extension.js
@@ -1,50 +1,55 @@
 /* global imports */
 
+const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
 const Util = imports.misc.util;
+const BoxPointer = imports.ui.boxpointer;
 
-let _shieldOnStatusChanged, _shieldActivate, _shieldLock, _shieldDeactivate;
+let _systemMenu, _shieldOnStatusChanged, _shieldActivate, _shieldLock, _shieldDeactivate;
 
-function _lock() { 
+function _lock() {
     Main.overview.hide();
-    Main.panel.closeQuickSettings();
+    _systemMenu.menu.itemActivated( BoxPointer.PopupAnimation.NONE );
     Util.spawn( ['gnome-screensaver-command', '-l'] );
 };
 
-function _unlock() { 
+function _unlock() {
     Util.spawn( ['gnome-screensaver-command', '-d'] );
 };
 
+function _reset_kbd_layout()
+{
+    const sourceman = imports.ui.status.keyboard.getInputSourceManager();
+
+    if ( !sourceman ) {
+        return;
+    }
+
+    const idx = sourceman.currentSource.index;
+
+    if ( idx !== 0 ) {
+        sourceman.inputSources[0].activate( true );
+    }
+}
+
 function enable()
 {
-    _shieldOnStatusChanged = null;
-    _shieldActivate = null;
-    _shieldLock = null;
-    _shieldDeactivate = null;
-    
-    let _lockActionBtn = null;
-    let arr = Main.panel.statusArea.quickSettings._system._systemItem.child.get_children();
-    for (i = 0; i < arr.length; i++) {
-        if ( arr[i].toString().includes( "LockItem" ) ) {
-            _lockActionBtn = arr[i];
-            break;
-        }
-    }
+    _systemMenu = Main.panel.statusArea['aggregateMenu']._system;
 
-    if ( _lockActionBtn )
-    {
-        _shieldOnStatusChanged = Main.screenShield._onStatusChanged;
-        _shieldActivate = Main.screenShield.activate;
-        _shieldLock = Main.screenShield.lock;
-        _shieldDeactivate = Main.screenShield.deactivate;
+    _shieldOnStatusChanged = Main.screenShield._onStatusChanged;
+    _shieldActivate = Main.screenShield.activate;
+    _shieldLock = Main.screenShield.lock;
+    _shieldDeactivate = Main.screenShield.deactivate;
 
-        Main.screenShield._onStatusChanged = (status) => {}; // prevents gnome shell message "error: Unable to lock: Lock was blocked by an application"  
-        Main.screenShield.activate = (animate) => { _lock() };
-        Main.screenShield.lock = (animate) => { _lock() };
-        Main.screenShield.deactivate = (animate) => { _unlock() };
+    Main.screenShield._onStatusChanged = (status) => {}; // prevents gnome shell message "error: Unable to lock: Lock was blocked by an application"
+    Main.screenShield.activate = (animate) => { _lock(); };
+    Main.screenShield.lock = (animate) => { _lock(); };
+    Main.screenShield.deactivate = (animate) => { _unlock(); };
 
-        _lockActionBtn.visible = true;
-    }
+    Gio.DBus.session.signal_subscribe( null, "org.gnome.ScreenSaver", "ActiveChanged", "/org/gnome/ScreenSaver", null,
+            Gio.DBusSignalFlags.NONE, (connection, sender, path, iface, signal, params) => {
+                _reset_kbd_layout();
+            } );
 }
 
 function disable()
@@ -62,4 +67,3 @@ function disable()
         Main.screenShield.deactivate = _shieldDeactivate;
     }
 }
-
